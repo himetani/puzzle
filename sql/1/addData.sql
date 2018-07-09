199e996cf128c5bb576ece6fb9a1524af0f856ee
himetani <takafumi_t1224@jcom.home.ne.jp>
2018-07-09 23:55:44 +0900

    1

---
+++ sql/1/addData.sql
@@ -0,0 +1,9 @@
+INSERT INTO FiscalYearTable1 (
+	fiscal_year,
+	start_date,
+	end_date
+)
+VALUES 
+( 1995, '1994-10-01','1995-09-30'),
+( 1996, '1995-10-01','1996-08-30'),
+( 1997, '1996-10-01','1997-09-30')
